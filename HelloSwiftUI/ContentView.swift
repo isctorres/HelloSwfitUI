3dc88bd1c77a2e334bcfd6071a897faee16b261a
isctorres <isctorres@gmail.com>
2020-02-22 12:02:51 -0600

    actualizacion

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -9,11 +9,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var tapCount = 0
+    //@State var tapCount = 0
+    //@State private var name = ""
+    let students = ["John","Doe","Alice"]
+    @State private var selectedStudent = 0
     var body: some View {
-        Button("Tap Count \(tapCount)"){
-            self.tapCount += 1
+        
+        /*Form{
+            TextField("Enter your Name", text: $name)
+            Text("Your name is \(name)")
+        }*/
+        NavigationView{
+            Form{
+                Picker("Select your Student",selection: $selectedStudent){
+                    ForEach(0 ..< students.count){
+                        Text(self.students[$0])
+                    }
+                }
+                
+                Text("You chose: Studen #\(students[selectedStudent])")
+            }
         }
+        
+        /*Button("Tap Count \(tapCount)"){
+            self.tapCount += 1
+        }*/
         
         /*Button(action:{
             print("Click del botón")
